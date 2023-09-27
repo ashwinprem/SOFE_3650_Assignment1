b20ceb1a67fa2a2b179ab35ff42997b0216620e6
NT <128177967+noto-21@users.noreply.github.com>
2023-09-27 18:00:23 -0400

    Maj. Update: Added .gitignore and reworked files

--- Design Patterns/Test/Main.java
+++ Design Patterns/Test/Main.java
@@ -9,12 +9,12 @@ public class Main
         GroceryStoreSupplier appleFactory = new AppleHarvester();
         GroceryProductFactory apple = appleFactory.gatherFruit();
 
-        System.out.println("A new apple was created\n");
+        System.out.println("\nA new apple was created");
 
         GroceryStoreSupplier bananaFactory = new BananaHarvester();
         GroceryProductFactory banana = bananaFactory.gatherFruit();
 
-        System.out.println("A new banana was created\n");
+        System.out.println("\nA new banana was created\n");
 
         System.out.println("Apple's cost: $" + df.format(apple.getPrice()));
         System.out.println("Banana's cost: $" + df.format(banana.getPrice()));
